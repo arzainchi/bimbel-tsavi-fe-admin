61aae9522f3f19f41d39979ed3edf1fa42cfabd1
Mohamad Arizal <arizalmhmd5@gmail.com>
2023-02-01 21:54:00 +0700

    add master parent

--- src/app/pages/master/parents/form-parent/form-parent.component.ts
+++ src/app/pages/master/parents/form-parent/form-parent.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-form-parent',
-  templateUrl: './form-parent.component.html',
-  styleUrls: ['./form-parent.component.scss']
+  selector: "app-form-parent",
+  templateUrl: "./form-parent.component.html",
+  styleUrls: ["./form-parent.component.scss"],
 })
 export class FormParentComponent implements OnInit {
+  @Output() aftersave = new EventEmitter<boolean>();
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
   }
 
+  save = () => {
+    this.back()
+  }
+
+  back = () => {
+    this.aftersave.emit();
+  };
 }
--- src/app/pages/master/parents/list-parent/list-parent.component.ts
+++ src/app/pages/master/parents/list-parent/list-parent.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { DataTableDirective } from "angular-datatables";
 
 @Component({
-  selector: 'app-list-parent',
-  templateUrl: './list-parent.component.html',
-  styleUrls: ['./list-parent.component.scss']
+  selector: "app-list-parent",
+  templateUrl: "./list-parent.component.html",
+  styleUrls: ["./list-parent.component.scss"],
 })
 export class ListParentComponent implements OnInit {
+  // breadcrum data
+  breadCrumbItems: Array<{}>;
 
-  constructor() { }
+  // datatable
+  @ViewChild(DataTableDirective) dtElement: DataTableDirective;
+  dtInstace: Promise<DataTables.Api>;
+  dtOptions: DataTables.Settings = {};
+
+  // inisiasi
+  isOpenForm: boolean = false;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.dtOptions = {
+      ordering: false,
+      searching: false,
+      lengthChange: false,
+    };
+
+    this.breadCrumbItems = [
+      { label: "Master" },
+      { label: "Wali Murid", active: true },
+    ];
   }
 
+  createParent = () => {
+    this.showForm(true);
+  };
+
+  showForm = (val: boolean) => [(this.isOpenForm = val)];
 }
